404d11fa2aecf05d55009591c698c8e4044628e8
mudcud <enokosher@outlook.com>
2024-01-19 16:57:03 +0300

    step8

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -1,45 +1,40 @@
 import 'package:appy/services/auth/auth_provider.dart';
 import 'package:appy/services/auth/auth_user.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 
 //auth service does not authentic only firebase, it takes a what you have in auth provider
 //in this code it only exposes same provider firebase to the outside world
 
-
 class AuthService implements AuthProvider{
   final AuthProvider provider;
+
   const AuthService(this.provider);
   
   @override
-  Future<AuthUser> currentUser({  
+  Future<AuthUser> createUser({
     required String email,
-    required String password
-    }) => 
-    provider.createUser(emailemail: email,
-    password: password,
-    ) ;
-
-    @override
-    AuthUser? get currentUser => provider.currentUser;
-
-    @override
-    Future<AuthUser> login({
-      required String email,
-      required String password,
-
-    })=>
-    provider.login(emailemail:email,
+    required String password,
+     })  => 
+    provider.createUser(email: email,
     password: password,
     );
-    @override
-    Future<void> logOut() => provider.logOut();
 
-      @override
-    Future<void> sendEmailVerification() => provider.sendEmailVerification();
+  @override
 
-    }
-
-
-
+  AuthUser? get currentUser => provider.currentUser;
+  
+  @override
+ Future<void> logOut() => provider.logOut();
+  
+  @override
+  Future<AuthUser> login({
+    required String email, 
+    required String password,
+    }) => provider.login(
+      email:email,
+      password: password,
+    );
+  
+  @override
+   Future<void> sendEmailVerification() => provider.sendEmailVerification();
 }
-
